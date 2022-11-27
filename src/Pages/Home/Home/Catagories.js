807a2e90e6742b6733bb51b264fc8fe249a3b65d
mostafiz <rafi11zxy@gmail.com>
2022-11-27 10:30:06 +0600

    catagory added and set dynamic data

--- src/Pages/Home/Home/Catagories.js
+++ src/Pages/Home/Home/Catagories.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Catagories = () => {
+  const [catagory, setCatagory] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => setCatagory(data));
+  }, []);
+
   return (
     <div>
       <div className="flex items-center justify-cente container mx-auto">
         <div className="w-full flex flex-col sm:flex-row gap-4">
-          <Link to="/" className="flex-1 bg-red-300 p-4 cursor-pointer text-center font-bold">
-            <p>BEDROOM</p>
-          </Link>
-          <Link className="flex-1 bg-yellow-300 p-4 cursor-pointer text-center font-bold">
-            <p>Sofa Cum Bed</p>
-          </Link>
-          <Link className="flex-1 bg-indigo-300 p-4 cursor-pointer text-center font-bold">
-            <p>Dining Set</p>
-          </Link>
+          {catagory?.map((catagories) => (
+            <Link
+              to={`/${catagories.catagory.replaceAll(" ","")}`}
+              className="flex-1 bg-purple-700
+               p-4 cursor-pointer text-center font-bold uppercase text-white"
+            >
+              <p>{catagories.catagory}</p>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
